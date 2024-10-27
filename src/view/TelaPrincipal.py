9008117c9c2f371729733c02db8808cda3e3d8f8
Marcos <marcosvieira120404@gmail.com>
2024-10-27 02:42:11 -0300

    fix

--- src/view/TelaPrincipal.py
+++ src/view/TelaPrincipal.py
@@ -1,4 +1,4 @@
-from assets.TelaPrincipal_ui import Ui_MainWindow
+from assets.ui.TelaPrincipal_ui import Ui_MainWindow
 from TelaMusica import TelaMusica
 
 from PySide6.QtWidgets import QMainWindow
