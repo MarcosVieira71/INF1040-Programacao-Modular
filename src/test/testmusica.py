2876124309bfc44943c48b49a5047c3e7eabae05
Marcos <marcosvieira@tecgraf.puc-rio.br>
2024-11-05 17:18:22 -0300

    testes de encontrarMusica adicionados

--- src/test/testmusica.py
+++ src/test/testmusica.py
@@ -48,6 +48,23 @@ def test_VerificaMusicaExistente():
 
 def test_VerificaMusicaInexistente():
     dicionarioMusicas = {}
+    adicionarMusica("src/test/musicas_teste/11 - Max Coveri - Running in the 90's.mp3", dicionarioMusicas)
     resultado = verificaMusica("Autor", "MusicaInexistente", dicionarioMusicas)
     assert resultado == {'codigo_retorno': 0, 'mensagem': 'Música não existe no dicionário'}
     
+def test_EncontrarMusicaExistente():
+    dicionarioMusicas = {}
+    adicionarMusica("src/test/musicas_teste/11 - Max Coveri - Running in the 90's.mp3", dicionarioMusicas)
+    resultado = encontrarMusica("Desconhecido", "11 - Max Coveri - Running in the 90's", dicionarioMusicas)
+    assert resultado == {'codigo_retorno': 1, 'mensagem': 'Música encontrada', 'musica': {
+        'duracao': 284.93,
+        'autor': 'Desconhecido',
+        'nome': "11 - Max Coveri - Running in the 90's",
+        'caminho': "src/test/musicas_teste/11 - Max Coveri - Running in the 90's.mp3"
+    }}
+
+def test_EncontrarMusicaInexistente():
+    dicionarioMusicas = {}
+    adicionarMusica("src/test/musicas_teste/11 - Max Coveri - Running in the 90's.mp3", dicionarioMusicas)
+    resultado = encontrarMusica("Autor", "MusicaInexistente", dicionarioMusicas)
+    assert resultado == {'codigo_retorno': 0, 'mensagem': 'Música não encontrada', 'musica': None}
