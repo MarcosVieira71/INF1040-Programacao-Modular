e66cb8854d36e13dec064686ab2e2c709ca95250
rafaribeiro2013 <rafaribeiro2013@gmail.com>
2024-11-20 12:52:51 -0300

    arquivos de teste e criação de funções
    troca do arquivo de testes do modulo playlist e criação das outras funções do modulo playlist

--- src/modulos/playlist.py
+++ src/modulos/playlist.py
@@ -19,16 +19,78 @@ def criarPlaylist(nomePlaylist:str, dicionarioPlaylists = dicionarioPlaylists):
     return resultado
 
 def adicionarMusicaNaPlaylist(nomePlaylist:str, nomeAutor:str, nomeMusica:str, dicionarioPlaylists = dicionarioPlaylists):
-    return 0
+    if nomePlaylist in dicionarioPlaylists:
+        musica_existe = musica.verificaMusica(nomeAutor, nomeMusica)
+        if musica_existe["codigo_retorno"] == 1:
+            dicionarioPlaylists[nomePlaylist].append((nomeAutor,nomeMusica))
+            resultado = {
+            'codigo_retorno': 1,
+            'mensagem': "Música adicionada com sucesso"
+            }
+        else:
+            resultado = {
+            'codigo_retorno': 0,
+            'mensagem': "Música não existe no dicionário"
+            }
+    else:
+        resultado = {
+        'codigo_retorno': -1,
+        'mensagem': "Playlist inexistente"
+        }
+    return resultado
 
 def excluirMusicaDaPlaylist(nomePlaylist:str, nomeAutor:str, nomeMusica:str, dicionarioPlaylists = dicionarioPlaylists):
-    return 0
+    if nomePlaylist in dicionarioPlaylists:
+        if (nomeAutor,nomeMusica) in dicionarioPlaylists[nomePlaylist]:
+            dicionarioPlaylists[nomePlaylist].remove((nomeAutor,nomeMusica))
+            resultado = {
+            'codigo_retorno': 1, 
+            'mensagem': 'Música removida da playlist'
+            }
+        else:
+            resultado = {
+            'codigo_retorno': 0, 
+            'mensagem': 'Música inexistente'
+            }
+    else:
+        resultado = {
+        'codigo_retorno': -1, 
+        'mensagem': 'Playlist inexistente'
+        } 
+    return resultado
 
 def verificarMusicaNaPlaylist(nomePlaylist:str, nomeAutor:str, nomeMusica:str, dicionarioPlaylists = dicionarioPlaylists):
-    return 0
+    if nomePlaylist in dicionarioPlaylists:
+        if (nomeAutor,nomeMusica) in dicionarioPlaylists[nomePlaylist]:
+            resultado = {
+            'codigo_retorno': 1, 
+            'mensagem': 'Música existe na playlist'
+            }
+        else:
+            resultado = {
+            'codigo_retorno': 0, 
+            'mensagem': 'Música não existe na playlist'
+            }
+    else:
+        resultado = {
+        'codigo_retorno': -1, 
+        'mensagem': 'Playlist inexistente'
+        } 
+    return resultado
 
 def excluirPlaylist(nomePlaylist:str, dicionarioPlaylists = dicionarioPlaylists):
-    return 0
+    if nomePlaylist in dicionarioPlaylists:
+        del dicionarioPlaylists[nomePlaylist]
+        resultado = {
+        'codigo_retorno': 1, 
+        'mensagem': 'Playlist excluída com sucesso'
+        }
+    else:
+        resultado = {
+        'codigo_retorno': 0, 
+        'mensagem': 'Playlist inexistente'
+        }
+    return resultado
 
 def mudaNomePlaylist(nomePlaylist:str, novoNome:str, dicionarioPlaylists = dicionarioPlaylists):
     if nomePlaylist in dicionarioPlaylists:
