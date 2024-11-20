2ce3b0d699e843743d957919d7ebf4a3559698e9
PedroVanini <pedrovaninilage@gmail.com>
2024-11-19 21:15:04 -0300

    Mais testes

--- src/test/test_texto.py
+++ src/test/test_texto.py
@@ -11,4 +11,19 @@ def test_geraTxtAvaliacoes_tipoCodificacaoInvalido():
     resultado = geraTxtAvaliacoes(lista_strings, tipo_codificacao)
 
     assert resultado == {"codigo_retorno": -1, "mensagem": "Tipo de codificação inválido"}
+#AINDA COM ERRO  
+def test_geraTxtAvaliacoes_utf8():
+    lista_strings = ["Avaliação 1: Muito bom!", "Avaliação 2: Excelente trabalho."]
+    tipo_codificacao = "UTF-8"
+    relatorios_dir = "src/relatorios"
+    caminho_utf8 = os.path.join(relatorios_dir, "avaliacoes_utf8.txt")
+    caminho_utf32 = os.path.join(relatorios_dir, "avaliacoes_utf32.txt")
+
+    resultado = geraTxtAvaliacoes(lista_strings, tipo_codificacao)
+    assert resultado == {"codigo_retorno": 1, "mensagem": "Relatório de avaliações gerado com sucesso"}
+    assert os.path.isfile(caminho_utf8)  
+    assert not os.path.isfile(caminho_utf32)  
     
+
+    
+    os.remove(caminho_utf8)
